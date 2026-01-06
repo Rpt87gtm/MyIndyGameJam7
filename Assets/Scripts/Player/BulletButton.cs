7d36819789fa45f8555e54efae5b1fbd93ce7b69
Rpt87gtm <rpt87gtm@bk.ru>
2026-01-06 19:58:35 +0800

    lock bullet count

--- Assets/Scripts/Player/BulletButton.cs
+++ Assets/Scripts/Player/BulletButton.cs
@@ -8,11 +8,18 @@ public class BulletButton : MonoBehaviour
 {
     public BulletType bulletType;
     public TextMeshProUGUI countText;
+    public Image bulletImage;
+
+    public Sprite emptySlot;
     private BulletsUI _bulletsUI;
     private Button _button;
+    private Sprite sprite;
+    private Image backgroundImage;
     void Awake()
     {
         _button = GetComponent<Button>();
+        backgroundImage = GetComponent<Image>();
+        sprite = backgroundImage.sprite;
     }
     public void Init(BulletsUI bulletsUI)
     {
@@ -26,16 +33,30 @@ public class BulletButton : MonoBehaviour
     }
     public void ShowCount(int count)
     {
+
         if (bulletType == BulletType.Normal)
         {
             countText.SetText("\u221E");
         }
+        else
+        {
+            if (count <= 0)
+            {
+                ShowLocked();
+                return;
+            }
+            countText.SetText($"x{count}");
+        }
+        bulletImage.color = new Color(1f, 1f, 1f, 1f);
+        backgroundImage.sprite = sprite;
         _button.interactable = true;
-        countText.SetText($"x{count}");
     }
     public void ShowLocked()
     {
         if (bulletType == BulletType.Normal) return;
+        bulletImage.color = new Color(1f, 1f, 1f, 0f);
+        countText.SetText("");
+        backgroundImage.sprite = emptySlot;
         _button.interactable = false;
     }
 }
